e65781bf4488c480e4ad1ee3a234a40dc0d99505
Amitaisela <68296620+Amitaisela@users.noreply.github.com>
2025-01-08 09:20:23 +0200

    Update main.py
    monotonic works questionably

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import os
 import itertools
 from Game import Game
 from AI import AI
-import math
 
 
 if __name__ == "__main__":
-    possible = ['m', 'u', 'e', 'g']
+    possible = ['u', 'e', 'g']
     heuristics = [''.join(p) for p in itertools.permutations(possible)]
     runs = 1000
+    filename = f"data/results_{heuristics[0]}_{runs}.csv"
     for h in heuristics:
         for i in range(runs):
             game = Game(4)
@@ -17,13 +17,15 @@ if __name__ == "__main__":
             while True:
                 message = ai.ChooseBestMove()
                 if message == "Won":
+                    # winning logic
                     break
                 if message == "Lost":
+                    # losing logic
                     break
 
-            if not os.path.exists(f'results_{runs}.csv'):
-                with open(f'results_{runs}.csv', 'w') as f:
+            if not os.path.exists(f'{filename}'):
+                with open(f'{filename}', 'w') as f:
                     f.write('heuristic,max,score,message\n')
 
-            with open(f'results_{runs}.csv', 'a') as f:
+            with open(f'{filename}', 'a') as f:
                 f.write(f'{h}, {game.max}, {game.official_score}, {message}\n')
